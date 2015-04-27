b8551001f5f58d1b09e5ba67256d5a2fe148fb38
Jackie <wangejay@gmail.com>
2015-04-27 17:01:25 +0800

    1st_release

---
+++ app/src/androidTest/java/eztofu_gpstracker/luffytech/com/eztofu_gpstracker/ApplicationTest.java
@@ -0,0 +1,13 @@
+package eztofu_gpstracker.luffytech.com.eztofu_gpstracker;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
